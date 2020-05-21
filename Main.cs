9725ea31abeb2d4e24c60c474ce4d92fd6ba9c90
octo <allblvnk@gmail.com>
2020-05-21 02:08:42 +0100

    Updated build number

--- Main.cs
+++ Main.cs
@@ -28,7 +28,7 @@ namespace AudicaModding
         public const string Name = "ScoreOverlay"; // Name of the Mod.  (MUST BE SET)
         public const string Author = "octo"; // Author of the Mod.  (Set as null if none)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.0.2"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "1.0.4"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 
